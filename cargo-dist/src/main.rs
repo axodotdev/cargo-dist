14ca0682457c79a8bdbd6577140801e6aa253d31
Aria Beingessner <a.beingessner@gmail.com>
2023-01-12 11:19:24 -0500

    various fixups to targets

--- cargo-dist/src/main.rs
+++ cargo-dist/src/main.rs
@@ -218,10 +218,10 @@ fn cmd_generate_ci(cli: &Cli, args: &GenerateCiArgs) -> Result<(), miette::Repor
 
 fn default_desktop_targets() -> Vec<String> {
     vec![
-        "x86_64-unknown-linux-gnu".to_owned(),
+        "x86_64-unknown-linux-musl".to_owned(),
         "x86_64-apple-darwin".to_owned(),
         "x86_64-pc-windows-msvc".to_owned(),
-        // cross-compile not yet supported
+        // cross-compiles not yet supported
         // "aarch64-gnu-unknown-linux".to_owned(),
         // "aarch64-pc-windows-msvc".to_owned(),
         // "aarch64-apple-darwin".to_owned(),
