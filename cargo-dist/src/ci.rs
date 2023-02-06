024195747445435bc6334d72d0094180b37bcf03
Antonios Barotsis <a.barotsis@student.tudelft.nl>
2023-02-06 17:38:31 +0100

    Set `ALL_CARGO_DIST_INSTALLER_ARGS` to `''`.

--- cargo-dist/src/ci.rs
+++ cargo-dist/src/ci.rs
@@ -205,6 +205,13 @@ fn write_github_ci(
         };
         write!(&mut installer_args, "--installer={installer} ")?;
     }
+
+    // If no installer args are present, add two single quotes to keep the YAML valid
+    // See https://github.com/axodotdev/cargo-dist/issues/101. 
+    if installer_args.is_empty() {
+      write!(&mut installer_args, "''")?;
+    }
+
     let installer_args = String::from_utf8(installer_args).unwrap();
     writeln!(f, "  ALL_CARGO_DIST_INSTALLER_ARGS: {installer_args}")?;
 
