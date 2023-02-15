bcba046919653e4dccd21433190b0688eb89c3c5
Aria Beingessner <a.beingessner@gmail.com>
2023-02-15 12:46:21 -0500

    initial attempt at new CI

--- cargo-dist/src/ci.rs
+++ cargo-dist/src/ci.rs
@@ -4,265 +4,235 @@
 
 use std::fs::File;
 
-use camino::Utf8PathBuf;
 use miette::{IntoDiagnostic, WrapErr};
-use tracing::warn;
+use semver::Version;
+use tracing::{info, warn};
 
-use crate::InstallerStyle;
-
-const GITHUB_CI_TRIGGER: &str = r###"# CI that:
-#
-# * checks for a Git Tag that looks like a release ("v1.2.0")
-# * creates a Github Release™
-# * builds binaries/packages with cargo-dist
-# * uploads those packages to the Github Release™
-#
-# Note that the Github Release™ will be created before the packages,
-# so there will be a few minutes where the release has no packages
-# and then they will slowly trickle in, possibly failing. To make
-# this more pleasant we mark the release as a "draft" until all
-# artifacts have been successfully uploaded. This allows you to
-# choose what to do with partial successes and avoids spamming
-# anyone with notifications before the release is actually ready.
-name: Release
-
-permissions:
-  contents: write
-
-# This task will run whenever you push a git tag that looks like
-# a version number. We just look for `v` followed by at least one number
-# and then whatever. so `v1`, `v1.0.0`, and `v1.0.0-prerelease` all work.
-#
-# If there's a prerelease-style suffix to the version then the Github Release™
-# will be marked as a prerelease (handled by taiki-e/create-gh-release-action).
-#
-# Note that when generating links to uploaded artifacts, cargo-dist will currently
-# assume that your git tag is always v{VERSION} where VERSION is the version in
-# the published package's Cargo.toml (this is the default behaviour of cargo-release).
-# In the future this may be made more robust/configurable.
-on:
-  push:
-    tags:
-      - v[0-9]+.*
-
-env:"###;
-
-const GITHUB_CI_CREATE_RELEASE: &str = r###"
-jobs:
-  # Create the Github Release™ so the packages have something to be uploaded to
-  create-release:
-    runs-on: ubuntu-latest
-    outputs:
-      tag: ${{ steps.create-gh-release.outputs.computed-prefix }}${{ steps.create-gh-release.outputs.version }}
-    steps:
-      - uses: actions/checkout@v3
-      - id: create-gh-release
-        uses: taiki-e/create-gh-release-action@v1
-        with:
-          draft: true
-          # (required) GitHub token for creating GitHub Releases.
-          token: ${{ secrets.GITHUB_TOKEN }}
-
-
-  # Build and packages all the things
-  upload-artifacts:
-    needs: create-release
-    strategy:
-      matrix:
-        # For these target platforms
-        include:"###;
-
-const GITHUB_CI_ARTIFACT_TASKS1: &str = r###"    runs-on: ${{ matrix.os }}
-    env:
-      GH_TOKEN: ${{ secrets.GITHUB_TOKEN }}
-    steps:
-      - uses: actions/checkout@v3
-      - name: Install Rust
-        run: rustup update stable && rustup default stable
-      - name: Install cargo-dist
-        run: ${{ matrix.install-dist }}
-      - name: Run cargo-dist
-        # This logic is a bit janky because it's trying to be a polyglot between
-        # powershell and bash since this will run on windows, macos, and linux!
-        # The two platforms don't agree on how to talk about env vars but they
-        # do agree on 'cat' and '$()' so we use that to marshal values between commands.
-        run: |
-          # Actually do builds and make zips and whatnot
-          cargo dist --target=${{ matrix.target }} --output-format=json > dist-manifest.json
-          echo "dist ran successfully"
-          cat dist-manifest.json
-          # Parse out what we just built and upload it to the Github Release™
-          cat dist-manifest.json | jq --raw-output ".releases[].artifacts[].path" > uploads.txt
-          echo "uploading..."
-          cat uploads.txt
-          gh release upload ${{ needs.create-release.outputs.tag }} $(cat uploads.txt)
-          echo "uploaded!"
-
-  # Compute and upload the manifest for everything
-  upload-manifest:
-    needs: create-release
-    runs-on: ubuntu-latest
-    env:
-      GH_TOKEN: ${{ secrets.GITHUB_TOKEN }}
-    steps:
-      - uses: actions/checkout@v3
-      - name: Install Rust
-        run: rustup update stable && rustup default stable
-      - name: Install cargo-dist"###;
-const GITHUB_CI_ARTIFACT_TASKS2: &str = r###"      - name: Run cargo-dist manifest
-        run: |
-          # Generate a manifest describing everything
-          cargo dist manifest --no-local-paths --output-format=json $ALL_CARGO_DIST_TARGET_ARGS $ALL_CARGO_DIST_INSTALLER_ARGS > dist-manifest.json
-          echo "dist manifest ran successfully"
-          cat dist-manifest.json
-          # Upload the manifest to the Github Release™
-          gh release upload ${{ needs.create-release.outputs.tag }} dist-manifest.json
-          echo "uploaded manifest!"
-          # Edit the Github Release™ title/body to match what cargo-dist thinks it should be
-          CHANGELOG_TITLE=$(cat dist-manifest.json | jq --raw-output ".releases[].changelog_title")
-          cat dist-manifest.json | jq --raw-output ".releases[].changelog_body" > new_dist_changelog.md
-          gh release edit ${{ needs.create-release.outputs.tag }} --title="$CHANGELOG_TITLE" --notes-file=new_dist_changelog.md
-          echo "updated release notes!""###;
-
-const GITHUB_CI_INSTALLERS: &str = r###"      - name: Run cargo-dist --installer=...
-        run: |
-          # Run cargo dist with --no-builds to get agnostic artifacts like installers
-          cargo dist --output-format=json --no-builds $ALL_CARGO_DIST_INSTALLER_ARGS > dist-manifest.json
-          echo "dist ran successfully"
-          cat dist-manifest.json
-          # Grab the installers that were generated and upload them.
-          # This filter is working around the fact that --no-builds is kinds hacky
-          # and still makes/reports malformed zips that we don't want to upload.
-          cat dist-manifest.json | jq --raw-output '.releases[].artifacts[] | select(.kind == "installer") | .path' > uploads.txt
-          echo "uploading..."
-          cat uploads.txt
-          gh release upload ${{ needs.create-release.outputs.tag }} $(cat uploads.txt)
-          echo "uploaded installers!""###;
-
-const GITHUB_CI_FINISH_RELEASE: &str = r###"
-  # Mark the Github Release™ as a non-draft now that everything has succeeded!
-  publish-release:
-    needs: [create-release, upload-artifacts, upload-manifest]
-    runs-on: ubuntu-latest
-    env:
-      GH_TOKEN: ${{ secrets.GITHUB_TOKEN }}
-    steps:
-      - uses: actions/checkout@v3
-      - name: mark release as non-draft
-        run: |
-          gh release edit ${{ needs.create-release.outputs.tag }} --draft=false
-"###;
+use crate::{DistGraph, SortedMap, SortedSet, TargetTriple};
 
 /// Generate CI for Github
 ///
 /// This actually creates a file and writes to disk!
-pub fn generate_github_ci(
-    workspace_dir: &Utf8PathBuf,
-    targets: &[String],
-    installers: &[InstallerStyle],
-) -> Result<(), miette::Report> {
+pub fn generate_github_ci(dist: &DistGraph) -> Result<(), miette::Report> {
     const GITHUB_CI_DIR: &str = ".github/workflows/";
     const GITHUB_CI_FILE: &str = "release.yml";
 
     // FIXME: should we try to avoid clobbering old files..?
-    let ci_dir = workspace_dir.join(GITHUB_CI_DIR);
+    let ci_dir = dist.workspace_dir.join(GITHUB_CI_DIR);
     let ci_file = ci_dir.join(GITHUB_CI_FILE);
+
+    info!("generating Github CI at {ci_file}");
     std::fs::create_dir_all(&ci_dir)
         .into_diagnostic()
         .wrap_err("Failed to create ci dir")?;
     let mut file = File::create(ci_file)
         .into_diagnostic()
         .wrap_err("Failed to create ci file")?;
-    write_github_ci(&mut file, targets, installers)
+    write_github_ci(&mut file, dist)
         .into_diagnostic()
         .wrap_err("Failed to write to CI file")?;
+    info!("successfully generated Github CI");
     Ok(())
 }
 
-fn write_github_ci(
-    f: &mut File,
-    targets: &[String],
-    installers: &[InstallerStyle],
-) -> Result<(), std::io::Error> {
-    use std::io::Write;
+/// Write the Github CI to something
+fn write_github_ci<W: std::io::Write>(f: &mut W, dist: &DistGraph) -> Result<(), std::io::Error> {
+    // If they don't specify a Rust version, just go for "stable"
+    let rust_version = dist.desired_rust_toolchain.as_deref().unwrap_or("stable");
 
-    writeln!(f, "{GITHUB_CI_TRIGGER}")?;
+    // If they don't specify a cargo-dist version, use this one
+    let self_dist_version = SELF_DIST_VERSION.parse().unwrap();
+    let dist_version = dist
+        .desired_cargo_dist_version
+        .as_ref()
+        .unwrap_or(&self_dist_version);
 
-    // Write out target args
-    let mut target_args = Vec::new();
-    for target in targets {
-        write!(&mut target_args, "--target={target} ")?;
-    }
-    let target_args = String::from_utf8(target_args).unwrap();
-    writeln!(f, "  ALL_CARGO_DIST_TARGET_ARGS: {target_args}")?;
-
-    // Write out the installer args
-    let mut installer_args = Vec::new();
-    for installer in installers {
-        let installer = match installer {
-            InstallerStyle::GithubShell => "github-shell",
-            InstallerStyle::GithubPowershell => "github-powershell",
-        };
-        write!(&mut installer_args, "--installer={installer} ")?;
+    // Figue out what builds we need to do
+    let mut needs_global_build = false;
+    let mut local_targets = SortedSet::new();
+    for release in &dist.releases {
+        if !release.global_artifacts.is_empty() {
+            needs_global_build = true;
+        }
+        local_targets.extend(release.targets.iter());
     }
 
-    // If no installer args are present, add two single quotes to keep the YAML valid
-    // See https://github.com/axodotdev/cargo-dist/issues/101.
-    if installer_args.is_empty() {
-        write!(&mut installer_args, "''")?;
+    // Install Rust with rustup (TODO: confirm this works)
+    let install_rust = format!("rustup update {rust_version} && rustup default {rust_version}");
+
+    // Get the platform-specific installation methods
+    let install_dist_sh = install_dist_sh_for_version(dist_version);
+    let install_dist_ps1 = install_dist_ps1_for_version(dist_version);
+
+    // Build up the task matrix for building Artifacts
+    let mut artifacts_matrix = String::new();
+
+    // If we have Global Artifacts, we need one task for that. If we've done a Good Job
+    // then these artifacts should be possible to build on *any* platform. Linux is usually
+    // fast/cheap, so that's a reasonable choice.s
+    if needs_global_build {
+        push_github_artifacts_matrix_entry(
+            &mut artifacts_matrix,
+            GITHUB_LINUX_RUNNER,
+            "--artifacts=global",
+            &install_dist_sh,
+        );
     }
 
-    let installer_args = String::from_utf8(installer_args).unwrap();
-    writeln!(f, "  ALL_CARGO_DIST_INSTALLER_ARGS: {installer_args}")?;
-
-    // Write out the current version
-    let dist_version = env!("CARGO_PKG_VERSION");
-    //writeln!(f, "  CARGO_DIST_VERSION: v{dist_version}")?;
-    let install_unix = format!("curl --proto '=https' --tlsv1.2 -L -sSf https://github.com/axodotdev/cargo-dist/releases/download/v{dist_version}/installer.sh | sh");
-    let install_windows = format!("irm 'https://github.com/axodotdev/cargo-dist/releases/download/v{dist_version}/installer.ps1' | iex");
-    // writeln!(f, "  CARGO_DIST_INSTALL_UNIX: {install_unix}")?;
-    // writeln!(f, "  CARGO_DIST_INSTALL_WINDOWS: {install_windows}")?;
-
-    writeln!(f, "{GITHUB_CI_CREATE_RELEASE}")?;
-
-    for target in targets {
-        let Some(os) = github_os_for_target(target) else {
-            warn!("skipping generating ci for {target} (no idea what github os should build this)");
-            continue;
-        };
-        writeln!(f, "        - target: {target}")?;
-        writeln!(f, "          os: {os}")?;
-        let install_cmd = if target.contains("windows") {
-            &install_windows
-        } else {
-            &install_unix
-        };
-        writeln!(f, "          install-dist: {install_cmd}")?;
+    // Figure out what Local Artifact tasks we need
+    let local_runs = distribute_targets_to_runners(local_targets);
+    for (runner, targets) in local_runs {
+        use std::fmt::Write;
+        let install_dist =
+            install_dist_for_github_runner(runner, &install_dist_sh, &install_dist_ps1);
+        let mut dist_args = String::from("--artifacts=local ");
+        for target in targets {
+            write!(dist_args, "--target={target} ").unwrap();
+        }
+        push_github_artifacts_matrix_entry(&mut artifacts_matrix, runner, &dist_args, install_dist);
     }
 
-    writeln!(f, "{GITHUB_CI_ARTIFACT_TASKS1}")?;
-    writeln!(f, "        run: {install_unix}")?;
-    writeln!(f, "{GITHUB_CI_ARTIFACT_TASKS2}")?;
+    // Finally write the final CI script to the Writer
+    let ci_yml = include_str!("ci.yml");
+    let ci_yml = ci_yml
+        .replace("{{{{INSTALL_RUST}}}}", &install_rust)
+        .replace("{{{{INSTALL_DIST_SH}}}}", &install_dist_sh)
+        .replace("{{{{ARTIFACTS_MATRIX}}}}", &artifacts_matrix);
 
-    if !installers.is_empty() {
-        writeln!(f, "{GITHUB_CI_INSTALLERS}")?;
-    }
+    f.write_all(ci_yml.as_bytes())?;
 
-    writeln!(f, "{GITHUB_CI_FINISH_RELEASE}")?;
     Ok(())
 }
 
-fn github_os_for_target(target: &str) -> Option<&'static str> {
+/// Add an entry to a Github Matrix (for the Artifacts tasks)
+fn push_github_artifacts_matrix_entry(
+    matrix: &mut String,
+    runner: &str,
+    dist_args: &str,
+    install_dist: &str,
+) {
+    use std::fmt::Write;
+
+    const MATRIX_ENTRY_TEMPLATE: &str = r###"
+        - os: {{{{GITHUB_RUNNER}}}}
+          dist-args: {{{{DIST_ARGS}}}}
+          install-dist: {{{{INSTALL_DIST}}}}"###;
+
+    let entry = MATRIX_ENTRY_TEMPLATE
+        .replace("{{{{GITHUB_RUNNER}}}}", runner)
+        .replace("{{{{DIST_ARGS}}}}", dist_args)
+        .replace("{{{{INSTALL_DIST}}}}", install_dist);
+
+    write!(matrix, "{}", entry).unwrap();
+}
+
+/// Given a set of targets we want to build local artifacts for, map them to Github Runners
+fn distribute_targets_to_runners<'a>(
+    targets: SortedSet<&'a TargetTriple>,
+) -> impl Iterator<Item = (GithubRunner, Vec<&'a TargetTriple>)> {
+    let mut groups = SortedMap::<GithubRunner, Vec<&'a TargetTriple>>::new();
+    for target in targets {
+        let runner = github_runner_for_target(target);
+        let runner = runner.unwrap_or_else(|| {
+            let default = GITHUB_LINUX_RUNNER;
+            warn!("not sure which github runner should be used for {target}, assuming {default}");
+            default
+        });
+        groups.entry(runner).or_default().push(target);
+    }
+    groups.into_iter()
+}
+
+/// A string representing a Github Runner
+type GithubRunner = &'static str;
+/// The Github Runner to use for Linux
+const GITHUB_LINUX_RUNNER: &str = "ubuntu-20.04";
+/// The Github Runner to use for macos
+const GITHUB_MACOS_RUNNER: &str = "macos-11";
+/// The Github Runner to use for windows
+const GITHUB_WINDOWS_RUNNER: &str = "windows-2019";
+
+/// Get the appropriate Github Runner for building a target
+fn github_runner_for_target(target: &TargetTriple) -> Option<GithubRunner> {
     // We want to default to older runners to minimize the places
     // where random system dependencies can creep in and be very
     // recent. This helps with portability!
     if target.contains("linux") {
-        Some("ubuntu-20.04")
+        Some(GITHUB_LINUX_RUNNER)
     } else if target.contains("apple") {
-        Some("macos-11")
+        Some(GITHUB_MACOS_RUNNER)
     } else if target.contains("windows") {
-        Some("windows-2019")
+        Some(GITHUB_WINDOWS_RUNNER)
     } else {
         None
     }
+}
+
+/// Select the cargo-dist installer approach for a given Github Runner
+fn install_dist_for_github_runner<'a>(
+    runner: GithubRunner,
+    install_sh: &'a str,
+    install_ps1: &'a str,
+) -> &'a str {
+    if runner == GITHUB_LINUX_RUNNER || runner == GITHUB_MACOS_RUNNER {
+        install_sh
+    } else if runner == GITHUB_WINDOWS_RUNNER {
+        install_ps1
+    } else {
+        unreachable!("internal error: unknown github runner!?")
+    }
+}
+
+/// The current version of cargo-dist
+const SELF_DIST_VERSION: &str = env!("CARGO_PKG_VERSION");
+/// The first epoch of cargo-dist, after this version a bunch of things changed
+/// and we don't support generating CI for that design anymore!
+const DIST_EPOCH_1: &str = "0.0.2";
+/// The base URL to fetch cargo-dist artifacts from (tag gets appended)
+const BASE_DIST_FETCH_URL: &str = "https://github.com/axodotdev/cargo-dist/releases/download";
+
+/// Get the command to invoke to install cargo-dist via sh script
+fn install_dist_sh_for_version(version: &Version) -> String {
+    if let Some(git) = install_dist_git(version) {
+        return git;
+    }
+    let epoch1 = Version::parse(DIST_EPOCH_1).unwrap();
+    let installer_name = if version > &epoch1 {
+        format!("cargo-dist-v{version}-installer.sh")
+    } else {
+        // FIXME: we should probably do this check way higher up and produce a proper err...
+        panic!("requested cargo-dist <= {DIST_EPOCH_1}, which is not supported by the this copy of cargo-dist ({SELF_DIST_VERSION})");
+    };
+
+    // FIXME: it would be nice if these values were somehow using all the machinery
+    // to compute these values for packages we build *BUT* it's messy and not that important
+    let installer_url = format!("{BASE_DIST_FETCH_URL}/v{version}/{installer_name}");
+    format!("curl --proto '=https' --tlsv1.2 -LsSf {installer_url} | sh")
+}
+
+/// Get the command to invoke to install cargo-dist via ps1 script
+fn install_dist_ps1_for_version(version: &Version) -> String {
+    if let Some(git) = install_dist_git(version) {
+        return git;
+    }
+    let epoch1 = Version::parse(DIST_EPOCH_1).unwrap();
+    let installer_name = if version > &epoch1 {
+        format!("cargo-dist-v{version}-installer.ps1")
+    } else {
+        // FIXME: we should probably do this check way higher up and produce a proper err...
+        panic!("requested cargo-dist <= {DIST_EPOCH_1}, which is not supported by this copy of cargo-dist ({SELF_DIST_VERSION})");
+    };
+
+    // FIXME: it would be nice if these values were somehow using all the machinery
+    // to compute these values for packages we build *BUT* it's messy and not that important
+    let installer_url = format!("{BASE_DIST_FETCH_URL}/v{version}/{installer_name}");
+    format!("irm  {installer_url} | iex")
+}
+
+/// Cute little hack for developing dist itself: if we see a version like "0.0.3-github-config"
+/// then install from the main github repo with branch=config!
+fn install_dist_git(version: &Version) -> Option<String> {
+    version.pre.strip_prefix("github-").map(|branch| {
+        format!("cargo install --git https://github.com/axodotdev/cargo-dist/ --branch={branch} cargo-dist")
+    })
 }
