a5238152a04a87c1031e9ca80aa5863f6ce453bc
Misty De Méo <misty@axo.dev>
2023-08-16 14:53:24 -0700

    feat: import repo tests

--- axoproject/src/repo.rs
+++ axoproject/src/repo.rs
@@ -108,3 +108,38 @@ impl GithubRepoInput {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_parses_an_https_repo_string() {
+        let input = "https://github.com/axodotdev/oranda";
+        let actual_owner = "axodotdev";
+        let actual_name = "oranda";
+        let parsed = GithubRepo::from_url(input).unwrap();
+        assert_eq!(parsed.owner, actual_owner);
+        assert_eq!(parsed.name, actual_name);
+    }
+
+    #[test]
+    fn it_parses_an_https_repo_string_with_dot_git() {
+        let input = "https://github.com/axodotdev/oranda.git";
+        let actual_owner = "axodotdev";
+        let actual_name = "oranda";
+        let parsed = GithubRepo::from_url(input).unwrap();
+        assert_eq!(parsed.owner, actual_owner);
+        assert_eq!(parsed.name, actual_name);
+    }
+
+    #[test]
+    fn it_parses_an_ssh_repo_string() {
+        let input = "git@github.com:axodotdev/oranda.git";
+        let actual_owner = "axodotdev";
+        let actual_name = "oranda";
+        let parsed = GithubRepo::from_url(input).unwrap();
+        assert_eq!(parsed.owner, actual_owner);
+        assert_eq!(parsed.name, actual_name);
+    }
+}
