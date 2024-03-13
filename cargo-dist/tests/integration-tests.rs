fe2933f92bc50992127fa68b81c609a2c7fa20f0
Aria Beingessner <a.beingessner@gmail.com>
2024-03-13 17:24:17 -0400

    chore: add lies test for axolotlsay (but use non-lies to start)

--- cargo-dist/tests/integration-tests.rs
+++ cargo-dist/tests/integration-tests.rs
@@ -91,7 +91,7 @@ path-guid = "BFD25009-65A4-4D1E-97F1-0030465D90D6"
         let ci_result = ctx.cargo_dist_generate(test_name)?;
         let ci_snap = ci_result.check_all()?;
         // Do usual build+plan checks
-        let main_result = ctx.cargo_dist_build_and_plan(test_name)?;
+        let main_result = ctx.cargo_dist_build_lies_and_plan(test_name)?;
         let main_snap = main_result.check_all(ctx, ".cargo/bin/")?;
         // snapshot all
         main_snap.join(ci_snap).snap();
