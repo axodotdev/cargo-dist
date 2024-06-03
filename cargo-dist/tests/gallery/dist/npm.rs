e6e89056bbbd8e3b01e004241f531b8b39afeed0
Aria Beingessner <a.beingessner@gmail.com>
2024-06-03 11:06:36 -0400

    chore: fmt

--- cargo-dist/tests/gallery/dist/npm.rs
+++ cargo-dist/tests/gallery/dist/npm.rs
@@ -38,28 +38,55 @@ impl DistResult {
         let parent = repo_dir.parent().unwrap();
 
         let tempdir = parent.join(format!("{repo_id}__{test_name}"));
-        runtest_npm(&ctx.tools.npm, tar, &tempdir, &package_tarball_path, app_name, bins)?;
-        runtest_pnpm(&ctx.tools.npm, &ctx.tools.pnpm, tar, &tempdir, &package_tarball_path, app_name, bins)?;
-        runtest_yarn(&ctx.tools.npm, &ctx.tools.yarn, tar, &tempdir, &package_tarball_path, app_name, bins)?;
+        runtest_npm(
+            &ctx.tools.npm,
+            tar,
+            &tempdir,
+            &package_tarball_path,
+            app_name,
+            bins,
+        )?;
+        runtest_pnpm(
+            &ctx.tools.npm,
+            &ctx.tools.pnpm,
+            tar,
+            &tempdir,
+            &package_tarball_path,
+            app_name,
+            bins,
+        )?;
+        runtest_yarn(
+            &ctx.tools.npm,
+            &ctx.tools.yarn,
+            tar,
+            &tempdir,
+            &package_tarball_path,
+            app_name,
+            bins,
+        )?;
 
         Ok(())
     }
 }
 
-fn runtest_yarn(npm: &Option<CommandInfo>, yarn: &Option<CommandInfo>, tar: &CommandInfo, tempdir: &Utf8Path, package_tarball_path: &Utf8Path, app_name: &str, bins: &[&str]) -> Result<()> {
-    let Some(npm) = npm else {
-        return Ok(())
-    };
-    let Some(yarn) = yarn else {
-        return Ok(())
-    };
+fn runtest_yarn(
+    npm: &Option<CommandInfo>,
+    yarn: &Option<CommandInfo>,
+    tar: &CommandInfo,
+    tempdir: &Utf8Path,
+    package_tarball_path: &Utf8Path,
+    app_name: &str,
+    bins: &[&str],
+) -> Result<()> {
+    let Some(npm) = npm else { return Ok(()) };
+    let Some(yarn) = yarn else { return Ok(()) };
 
-    clear_tempdir(&tempdir);
+    clear_tempdir(tempdir);
 
     // Have npm install/unpack the tarball to a project
     eprintln!("running npm install...");
     let parent_package_dir = tempdir.to_owned();
-    install_tarball_package(yarn, &parent_package_dir, &package_tarball_path)?;
+    install_tarball_package(yarn, &parent_package_dir, package_tarball_path)?;
 
     // Run the installed app
     eprintln!("npm exec'ing installed app...");
@@ -67,28 +94,31 @@ fn runtest_yarn(npm: &Option<CommandInfo>, yarn: &Option<CommandInfo>, tar: &Com
 
     // Now let's hop into the installed package and have it lint itself
     eprintln!("linting installed app...");
-    unpack_tarball_package(tar, &parent_package_dir, &package_tarball_path)?;
+    unpack_tarball_package(tar, &parent_package_dir, package_tarball_path)?;
     let package_dir = parent_package_dir.join("package");
     lint_package(npm, &package_dir, app_name)?;
 
     Ok(())
 }
 
+fn runtest_pnpm(
+    npm: &Option<CommandInfo>,
+    pnpm: &Option<CommandInfo>,
+    tar: &CommandInfo,
+    tempdir: &Utf8Path,
+    package_tarball_path: &Utf8Path,
+    app_name: &str,
+    bins: &[&str],
+) -> Result<()> {
+    let Some(npm) = npm else { return Ok(()) };
+    let Some(pnpm) = pnpm else { return Ok(()) };
 
-fn runtest_pnpm(npm: &Option<CommandInfo>, pnpm: &Option<CommandInfo>, tar: &CommandInfo, tempdir: &Utf8Path, package_tarball_path: &Utf8Path, app_name: &str, bins: &[&str]) -> Result<()> {
-    let Some(npm) = npm else {
-        return Ok(())
-    };
-    let Some(pnpm) = pnpm else {
-        return Ok(())
-    };
-
-    clear_tempdir(&tempdir);
+    clear_tempdir(tempdir);
 
     // Have npm install/unpack the tarball to a project
     eprintln!("running npm install...");
     let parent_package_dir = tempdir.to_owned();
-    install_tarball_package(pnpm, &parent_package_dir, &package_tarball_path)?;
+    install_tarball_package(pnpm, &parent_package_dir, package_tarball_path)?;
 
     // Run the installed app
     eprintln!("npm exec'ing installed app...");
@@ -96,25 +126,29 @@ fn runtest_pnpm(npm: &Option<CommandInfo>, pnpm: &Option<CommandInfo>, tar: &Com
 
     // Now let's hop into the installed package and have it lint itself
     eprintln!("linting installed app...");
-    unpack_tarball_package(tar, &parent_package_dir, &package_tarball_path)?;
+    unpack_tarball_package(tar, &parent_package_dir, package_tarball_path)?;
     let package_dir = parent_package_dir.join("package");
     lint_package(npm, &package_dir, app_name)?;
 
     Ok(())
 }
 
+fn runtest_npm(
+    npm: &Option<CommandInfo>,
+    tar: &CommandInfo,
+    tempdir: &Utf8Path,
+    package_tarball_path: &Utf8Path,
+    app_name: &str,
+    bins: &[&str],
+) -> Result<()> {
+    let Some(npm) = npm else { return Ok(()) };
 
-fn runtest_npm(npm: &Option<CommandInfo>, tar: &CommandInfo, tempdir: &Utf8Path, package_tarball_path: &Utf8Path, app_name: &str, bins: &[&str]) -> Result<()> {
-    let Some(npm) = npm else {
-        return Ok(())
-    };
-
-    clear_tempdir(&tempdir);
+    clear_tempdir(tempdir);
 
     // Have npm install/unpack the tarball to a project
     eprintln!("running npm install...");
     let parent_package_dir = tempdir.to_owned();
-    install_tarball_package(npm, &parent_package_dir, &package_tarball_path)?;
+    install_tarball_package(npm, &parent_package_dir, package_tarball_path)?;
 
     // Run the installed app
     eprintln!("npm exec'ing installed app...");
@@ -122,7 +156,7 @@ fn runtest_npm(npm: &Option<CommandInfo>, tar: &CommandInfo, tempdir: &Utf8Path,
 
     // Now let's hop into the installed package and have it lint itself
     eprintln!("linting installed app...");
-    unpack_tarball_package(tar, &parent_package_dir, &package_tarball_path)?;
+    unpack_tarball_package(tar, &parent_package_dir, package_tarball_path)?;
     let package_dir = parent_package_dir.join("package");
     lint_package(npm, &package_dir, app_name)?;
 
@@ -131,9 +165,9 @@ fn runtest_npm(npm: &Option<CommandInfo>, tar: &CommandInfo, tempdir: &Utf8Path,
 
 fn clear_tempdir(tempdir: &Utf8Path) {
     if tempdir.exists() {
-        std::fs::remove_dir_all(&tempdir).unwrap();
+        std::fs::remove_dir_all(tempdir).unwrap();
     }
-    std::fs::create_dir_all(&tempdir).unwrap();
+    std::fs::create_dir_all(tempdir).unwrap();
 }
 
 fn install_tarball_package(
