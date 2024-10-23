074d590ea2db0f66939a2f327456ce24d6cbd6ee
Amos Wenger <amos@bearcove.net>
2024-10-23 16:47:37 +0200

    Censor more things in hash file (which is getting ridiculous)

--- cargo-dist/tests/gallery/dist/snapshot.rs
+++ cargo-dist/tests/gallery/dist/snapshot.rs
@@ -169,8 +169,12 @@ pub fn snapshot_settings_with_gallery_filter() -> insta::Settings {
         r#""build_environment": "indeterminate""#,
     );
     settings.add_filter(
-        r"[0-9a-f]{64} ([a-zA-Z0-9-]+)-npm-package\.tar\.gz",
-        "CENSORED (see https://github.com/axodotdev/cargo-dist/issues/1477) npm-package.tar.gz",
+        r"[0-9a-f]{64} ([a-zA-Z0-9-_]+)\.tar\.gz",
+        "CENSORED (see https://github.com/axodotdev/cargo-dist/issues/1477) CENSORED.tar.gz",
+    );
+    settings.add_filter(
+        r"[0-9a-f]{64} ([a-zA-Z0-9-_]+)\.pkg",
+        "CENSORED (see https://github.com/axodotdev/cargo-dist/issues/1477) CENSORED.pkg",
     );
     settings
 }
