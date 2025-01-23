e1ea145d2a6b2eb4ae56b9d2fe7366bd38234d02
Ellen Marie Dash <me@duckie.co>
2025-01-23 15:12:39 -0500

    Fix clippy warnings

--- cargo-dist/src/init/mod.rs
+++ cargo-dist/src/init/mod.rs
@@ -1,8 +1,8 @@
 pub(crate) mod v0;
 pub use v0::do_init;
 pub mod console_helpers;
-mod init_args;
 mod dist_profile;
+mod init_args;
 
-pub use init_args::InitArgs;
 pub use dist_profile::init_dist_profile;
+pub use init_args::InitArgs;
