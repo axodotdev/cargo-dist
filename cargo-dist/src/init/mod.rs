9c3a01d80174315bf6d7e44241abee00e39ebdd7
Ellen Marie Dash <me@duckie.co>
2025-01-23 15:09:29 -0500

    Rename init/init_dist_profile.rs -> init/dist_profile.rs

--- cargo-dist/src/init/mod.rs
+++ cargo-dist/src/init/mod.rs
@@ -2,7 +2,7 @@ pub(crate) mod v0;
 pub use v0::do_init;
 pub mod console_helpers;
 mod init_args;
-mod init_dist_profile;
+mod dist_profile;
 
 pub use init_args::InitArgs;
-pub use init_dist_profile::init_dist_profile;
+pub use dist_profile::init_dist_profile;
