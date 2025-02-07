716f2cc708db40ae2bbd39151f46a9d665eecc40
Ellen Marie Dash <me@duckie.co>
2025-02-07 16:34:45 -0500

    Finish apply_dist::artifacts::apply()

--- cargo-dist/src/init/apply_dist/artifacts.rs
+++ cargo-dist/src/init/apply_dist/artifacts.rs
@@ -2,31 +2,29 @@ use super::helpers::*;
 use crate::config::v1::artifacts::archives::ArchiveLayer;
 use crate::config::v1::artifacts::ArtifactLayer;
 use crate::config::v1::layer::{BoolOr, BoolOrOptExt};
-use axoasset::toml_edit;
+use axoasset::toml_edit::{self, DocumentMut, Item, Table};
 
 pub fn apply(table: &mut toml_edit::Table, artifacts: &Option<ArtifactLayer>) {
     let Some(artifacts) = artifacts else {
         return;
     };
-    let Some(artifacts_table) = table.get_mut("artifacts") else {
-        return;
-    };
-    let toml_edit::Item::Table(artifacts_table) = artifacts_table else {
-        panic!("Expected [dist.artifacts] to be a table");
-    };
-
-    // TODO(migration): implement this
+    let artifacts_table = table
+        .entry("artifacts")
+        .or_insert(Item::Table(Table::new()))
+        .as_table_mut()
+        .expect("[dist.artifacts] should be a table");
 
     apply_artifacts_archives(artifacts_table, &artifacts.archives);
 
     apply_optional_value(
         artifacts_table,
         "source-tarball",
-        "# Generate and dist a source tarball\n",
+        "# Generate a source tarball\n",
         artifacts.source_tarball,
     );
 
-    // TODO(migration): implement dist.artifacts.extra.
+    // FIXME(migration): Like with the v0 config, [dist.artifacts.extra] is
+    // not currently reformatted due to compelxity.
     /*
     apply_optional_value(
         artifacts_table,
@@ -56,12 +54,11 @@ fn apply_artifacts_archives(
     let Some(archives) = archives else {
         return;
     };
-    let Some(archives_table) = artifacts_table.get_mut("archives") else {
-        return;
-    };
-    let toml_edit::Item::Table(archives_table) = archives_table else {
-        panic!("Expected [dist.artifacts.archives] to be a table");
-    };
+    let archives_table = artifacts_table
+        .entry("archives")
+        .or_insert(Item::Table(Table::new()))
+        .as_table_mut()
+        .expect("[dist.artifacts.archives] should be a table");
 
     apply_string_list(
         archives_table,
@@ -97,4 +94,99 @@ fn apply_artifacts_archives(
         "# Which kinds of built libraries to include in the final archives\n",
         archives.package_libraries.as_ref(),
     );
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::{CompressionImpl, ChecksumStyle, ZipStyle};
+    use crate::config::LibraryStyle;
+    use axoasset::toml_edit::{self, DocumentMut, Item, Table};
+    use miette::IntoDiagnostic;
+    use pretty_assertions::{assert_eq, assert_ne};
+
+    fn source() -> toml_edit::DocumentMut {
+        let src = axoasset::SourceFile::new("fake-dist-workspace.toml", String::new());
+        let doc = src.deserialize_toml_edit().into_diagnostic().unwrap();
+        doc
+    }
+
+    // Given a DocumentMut, make sure it has a [dist] table, and return
+    // a reference to that dist table.
+    fn dist_table(doc: &mut toml_edit::DocumentMut) -> &mut toml_edit::Table {
+        let dist = doc
+            .entry("dist")
+            .or_insert(Item::Table(Table::new()))
+            .as_table_mut()
+            .unwrap();
+        // Don't show the empty top-level [dist].
+        dist.set_implicit(true);
+        // Return the table we just created.
+        dist
+    }
+
+    #[test]
+    fn apply_artifacts_empty() {
+        let expected = "";
+
+        let artifacts = Some(ArtifactLayer {
+            archives: None,
+            checksum: None,
+            extra: None,
+            source_tarball: None,
+        });
+
+        let mut doc = source();
+        let table = dist_table(&mut doc);
+
+        apply(table, &artifacts);
+
+        let toml_text = table.to_string();
+        assert_eq!(toml_text, expected);
+    }
+
+    #[test]
+    fn apply_artifacts_everything() {
+        let expected = r#"
+# Artifact configuration for dist
+[dist.artifacts]
+# Generate a source tarball
+source-tarball = false
+# The checksum format to generate
+checksum = "blake2b"
+
+[dist.artifacts.archives]
+# Extra static files to include in each App (path relative to this Cargo.toml's dir)
+include = ["some-include"]
+# Whether to auto-include files like READMEs, LICENSEs, and CHANGELOGs (default true)
+auto-includes = false
+# The archive format to use for windows builds (defaults .zip)
+windows-archive = ".tar.gz"
+# The archive format to use for non-windows builds (defaults .tar.xz)
+unix-archive = ".zip"
+# Which kinds of built libraries to include in the final archives
+package-libraries = ["cdylib", "cstaticlib"]
+"#;
+
+        let artifacts = Some(ArtifactLayer {
+            archives: Some(ArchiveLayer {
+                include: Some(vec!["some-include".into()]),
+                auto_includes: Some(false),
+                windows_archive: Some(ZipStyle::Tar(CompressionImpl::Gzip)),
+                unix_archive: Some(ZipStyle::Zip),
+                package_libraries: Some(vec![LibraryStyle::CDynamic, LibraryStyle::CStatic]),
+            }),
+            checksum: Some(ChecksumStyle::Blake2b),
+            extra: None,
+            source_tarball: Some(false),
+        });
+
+        let mut doc = source();
+        let table = dist_table(&mut doc);
+
+        apply(table, &artifacts);
+
+        let toml_text = doc.to_string();
+        assert_eq!(expected, toml_text);
+    }
 }
