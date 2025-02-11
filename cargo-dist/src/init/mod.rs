3e59768a5298e9d6401fd013db0a3e6940a26bc4
Ellen Marie Dash <me@duckie.co>
2025-02-11 12:50:17 -0500

    Comment out currently-unused use statements that I will likely need later.

--- cargo-dist/src/init/mod.rs
+++ cargo-dist/src/init/mod.rs
@@ -5,11 +5,13 @@ pub mod console_helpers;
 mod dist_profile;
 mod init_args;
 
+/*
 use crate::config::{self, v1::TomlLayer, Config};
 use crate::errors::DistResult;
 use crate::migrate;
 use crate::SortedMap;
 use crate::{do_generate, GenerateArgs};
 use console_helpers::theme;
+*/
 pub use dist_profile::init_dist_profile;
 pub use init_args::InitArgs;
