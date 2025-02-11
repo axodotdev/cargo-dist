0317e45477acf63f43b8b5d2ade85ccc180bbaed
Ellen Marie Dash <me@duckie.co>
2025-02-11 17:25:48 -0500

    [BROKEN, WIP] more work on v1 do_init()

--- cargo-dist/src/init/mod.rs
+++ cargo-dist/src/init/mod.rs
@@ -6,6 +6,7 @@ mod dist_profile;
 mod init_args;
 mod interactive;
 
+use axoproject::WorkspaceGraph;
 use console_helpers::theme;
 use crate::{do_generate, GenerateArgs};
 use crate::SortedMap;
@@ -14,15 +15,23 @@ use crate::errors::DistResult;
 use crate::migrate;
 pub use dist_profile::init_dist_profile;
 pub use init_args::InitArgs;
+use serde::Deserialize;
 
-/// Run 'dist init'
-pub fn do_init(cfg: &Config, args: &InitArgs) -> DistResult<()> {
-    if !config::want_v1()? {
-        return do_init_v0(cfg, args);
-    }
+/// Input for --with-json-config
+///
+/// Contains a TomlLayer (V1 equivalent of DistMetadata) for [dist] and
+/// then optionally ones for each package.
+#[derive(Debug, Deserialize)]
+#[serde(deny_unknown_fields)]
+struct MultiDistMetadata {
+    /// `[dist]`
+    workspace: TomlLayer,
+    /// package_name => `[package]`
+    #[serde(default)]
+    packages: SortedMap<String, TomlLayer>,
+}
 
-    let ctrlc_handler = console_helpers::ctrlc_handler();
-
+fn migrate_if_needed(cfg: &Config, args: &InitArgs) -> DistResult<()> {
     if migrate::needs_migration()? && !args.yes {
         let prompt = r#"Would you like to opt in to the new configuration format?
     Future versions of dist will feature major changes to the configuration format."#;
@@ -37,13 +46,10 @@ pub fn do_init(cfg: &Config, args: &InitArgs) -> DistResult<()> {
         }
     }
 
-    eprintln!("let's setup your dist config...");
-    eprintln!();
+    Ok(())
+}
 
-    let check = console_helpers::checkmark();
-
-    let workspaces = config::get_project()?;
-
+fn initialize_cargo_profile_if_needed(workspaces: &WorkspaceGraph) -> DistResult<()> {
     // For each [workspace] Cargo.toml in the workspaces, initialize [profile]
     let mut did_add_profile = false;
     for workspace_idx in workspaces.all_workspace_indices() {
@@ -57,44 +63,29 @@ pub fn do_init(cfg: &Config, args: &InitArgs) -> DistResult<()> {
     }
 
     if did_add_profile {
+        let check = console_helpers::checkmark();
         eprintln!("{check} added [profile.dist] to your workspace Cargo.toml");
     }
 
+    Ok(())
+}
 
-    // TODO(migration): should this handle JSON stuff? if so, what does that
-    // look like now that DistMetadata isn't being used?
-    let meta = interactive::get_new_metadata(cfg, args, &workspaces)?;
-    let mut workspace = meta;
-    let mut packages: SortedMap<String, TomlLayer> = SortedMap::new();
+fn collect_metadata(cfg: &Config, args: &InitArgs, workspaces: &WorkspaceGraph) -> DistResult<MultiDistMetadata> {
+    let workspace = interactive::get_new_metadata(cfg, args, &workspaces)?;
+    let packages: SortedMap<String, TomlLayer> = SortedMap::new();
 
-    // We're past the final dialoguer call; we can remove the ctrl-c handler.
-    ctrlc_handler.abort();
+    Ok(MultiDistMetadata {
+        workspace,
+        packages,
+    })
+}
 
-    let root_workspace = workspaces.root_workspace();
-
-    // Load in the root workspace toml to edit and write back
-    let mut workspace_toml = config::load_toml(&root_workspace.manifest_path)?;
-
-    apply_dist::apply_dist_to_workspace_toml(&mut workspace_toml, &workspace);
-
-    eprintln!();
-
-    let filename = root_workspace
-        .manifest_path
-        .file_name()
-        .unwrap_or("dist-workspace.toml");
-    let destination = root_workspace.manifest_path.to_owned();
-
-    // Save the workspace toml (potentially an effective no-op if we made no edits)
-    config::write_toml(&destination, workspace_toml)?;
-    let key = "[dist]";
-    eprintln!("{check} added {key} to your root {filename}");
-
+fn update_cargo_packages_if_needed(workspaces: &WorkspaceGraph, multi_meta: &MultiDistMetadata) -> DistResult<()> {
     // Now that we've done the stuff that's definitely part of the root Cargo.toml,
     // Optionally apply updates to packages
     for (_idx, package) in workspaces.all_packages() {
         // Gather up all the things we'd like to be written to this file
-        let meta = packages.get(&package.name);
+        let meta = multi_meta.packages.get(&package.name);
         let needs_edit = meta.is_some();
 
         if needs_edit {
@@ -119,6 +110,68 @@ pub fn do_init(cfg: &Config, args: &InitArgs) -> DistResult<()> {
             }
         }
     }
+
+    Ok(())
+}
+
+/// Run 'dist init'
+pub fn do_init(cfg: &Config, args: &InitArgs) -> DistResult<()> {
+    if !config::want_v1()? {
+        return do_init_v0(cfg, args);
+    }
+
+    // The flow for `dist init` is:
+    // 1. `dist migrate` if needed
+    // 2. fetch workspace config
+    // 3. initialize Cargo.toml [profile] tables if needed
+    // 4. collect metadata
+    // 5. apply config changes in-memory
+    // 6. write config to file
+    // 7. `dist generate` if needed
+
+    let ctrlc_handler = console_helpers::ctrlc_handler();
+    let check = console_helpers::checkmark();
+
+    // 1. run `dist migrate` if we're on a v0 config.
+    migrate_if_needed(cfg, args)?;
+
+    eprintln!("let's setup your dist config...");
+    eprintln!();
+
+    // 2. fetch workspace config.
+    let workspaces = config::get_project()?;
+
+    // 3. initialize Cargo.toml [profile] tables, if needed.
+    initialize_cargo_profile_if_needed(&workspaces)?;
+
+    // 4. collect metadata.
+    let multi_meta = collect_metadata(cfg, args, &workspaces)?;
+
+    // We're past the final dialoguer call; we can remove the ctrl-c handler.
+    ctrlc_handler.abort();
+
+    let root_workspace = workspaces.root_workspace();
+
+    // Load in the root workspace toml to edit and write back
+    let mut workspace_toml = config::load_toml(&root_workspace.manifest_path)?;
+
+    apply_dist::apply_dist_to_workspace_toml(&mut workspace_toml, &multi_meta.workspace);
+
+    eprintln!();
+
+    let filename = root_workspace
+        .manifest_path
+        .file_name()
+        .unwrap_or("dist-workspace.toml");
+    let destination = root_workspace.manifest_path.to_owned();
+
+    // Save the workspace toml (potentially an effective no-op if we made no edits)
+    config::write_toml(&destination, workspace_toml)?;
+    let key = "[dist]";
+    eprintln!("{check} added {key} to your root {filename}");
+
+    // 3. initialize Cargo.toml [profile] tables if needed
+    update_cargo_packages_if_needed(&workspaces, &multi_meta)?;
 
     eprintln!("{check} dist is setup!");
     eprintln!();
