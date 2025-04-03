47d5515cd112c42b24b844babbedfcb831d0d42b
Aria Desires <aria.desires@gmail.com>
2025-04-03 15:26:18 -0400

    reignore test

--- cargo-dist/tests/cli-tests.rs
+++ cargo-dist/tests/cli-tests.rs
@@ -267,6 +267,7 @@ fn generate_installer(version: &axotag::Version, release_type: ReleaseSourceType
 }
 
 #[test]
+#[ignore = "mysterious GITHUB_TOKEN permission issues abound"]
 fn test_self_update() {
     // Only do this if RUIN_MY_COMPUTER_WITH_INSTALLERS is set
     if std::env::var(ENV_RUIN_ME)
