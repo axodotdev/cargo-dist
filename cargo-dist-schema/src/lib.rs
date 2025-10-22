28ce3b79758e3736c9a9874130b6017c09d5e039
Misty De Meo <mistydemeo@gmail.com>
2025-10-21 18:23:14 -0700

    fix: schema changes for schemars 1.x

--- cargo-dist-schema/src/lib.rs
+++ cargo-dist-schema/src/lib.rs
@@ -953,7 +953,7 @@ impl DistManifest {
     }
 
     /// Get the JSON Schema for a DistManifest
-    pub fn json_schema() -> schemars::schema::RootSchema {
+    pub fn json_schema() -> schemars::Schema {
         schemars::schema_for!(DistManifest)
     }
 
